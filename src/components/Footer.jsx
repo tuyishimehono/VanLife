0575d7a69d18e3bb3fc404c12e3d8acee62dcf78
tuyishimehono <tuhonori1@gmail.com>
2024-08-06 17:01:00 +0200

    Adjust footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer() {
     return (
-        <footer className="text-stone-400 font-light bg-black text-3xl flex justify-center w-full py-3">&#169; 2022 #VANLIFE</footer>
+        <footer className="text-stone-400 font-light bg-black text-3xl flex justify-center w-full py-3 mt-auto">&#169; 2022 #VANLIFE</footer>
     )
 }
