f7ce0ece66ac7814a157799b48bce6ea512e4ccf
tuyishimehono <tuhonori1@gmail.com>
2024-08-09 19:45:52 +0200

    Adjust Vans.jsx

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 
-
 export default function Vans() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = useState([]);
